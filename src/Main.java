36a40a04723a2a1527dc5d6d5d88f976f70177db
Dj kim <jfy212@gmail.com>
2024-11-03 14:01:33 +0900

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String [] args){
-        System.out.println("이것은 master 브랜치. 최종 코드 올리는 곳");;
+        System.out.println("이것은 develop 브랜치.. 개발한 코드 모아놓는 곳");
+        System.out.println("여기서 모든 코드를 합치자");
     }
 }
